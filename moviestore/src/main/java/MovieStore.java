dfc61c001668541f61187c42cffa35ebe84062bb
Israel Mesa <mesa.israel50@gmail.com>
2018-08-17 18:16:46 -0400

    Improving out implementation

--- moviestore/src/main/java/MovieStore.java
+++ moviestore/src/main/java/MovieStore.java
@@ -1,12 +1,33 @@
 import model.Movie;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MovieStore {
 
+    List<Movie> movies = new LinkedList<Movie>();
+
     public List<Movie> findByPartialTitle(String partialTitle) {
 
+        List<Movie> result = new LinkedList<Movie>();
 
-        return null;
+        for (Movie movie : movies) {
+
+            if (movie.title().toUpperCase().contains(partialTitle.toUpperCase())) {
+
+                result.add(movie);
+
+            }
+
+        }
+        
+        return result;
+
+    }
+
+    public void add(Movie movie) {
+
+        movies.add(movie);
+
     }
 }
--- moviestore/src/test/java/MovieStoreTest.java
+++ moviestore/src/test/java/MovieStoreTest.java
@@ -1,18 +1,41 @@
 import model.Movie;
+import org.junit.Test;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public class MovieStoreTest {
 
+    @Test
     public void returnsNoResultsWhenNoTitlesPartiallyMatchSearch() throws Exception {
         MovieStore movieStore = new MovieStore();
 
         List<Movie> results = movieStore.findByPartialTitle("abc");
 
         assertThat(results.size(), is(0));
+
+    }
+
+    @Test
+    public void findMoviesWhenTitlesArePartiallyMatched() throws Exception {
+        MovieStore movieStore = new MovieStore();
+        Movie harryPotter =  new Movie("Harry Potter");
+        Movie starWars =  new Movie("Star Wars");
+        Movie starTrek =  new Movie("STAR Trek");
+
+
+        movieStore.add(harryPotter);
+        movieStore.add(new Movie("Shawshank Redemption"));
+        movieStore.add(starWars);
+        movieStore.add(starTrek);
+
+        List<Movie> results = movieStore.findByPartialTitle("tar");
+
+        assertThat(results.size(), is(2));
+        assertThat(results, containsInAnyOrder(starTrek, starWars));
 
     }
 
