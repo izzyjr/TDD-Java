33cb08e45b2a714335d9e0d9fbc37b5cf2eb5561
Israel Mesa <mesa.israel50@gmail.com>
2018-08-17 17:07:45 -0400

    Our first proper implementation

--- moviestore/src/main/java/model/Movie.java
+++ moviestore/src/main/java/model/Movie.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Movie {
+    private String title;
+
+    public Movie(String title) {
+
+        this.title = title;
+    }
+
+    public String title() {
+
+        return title;
+
+    }
 }
