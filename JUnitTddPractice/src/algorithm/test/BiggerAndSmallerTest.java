3524e7446a407d55b901ac0c773fbbb5a75bda52
Israel Mesa <mesa.israel50@gmail.com>
2018-11-14 19:17:34 -0500

    update - 1

--- JUnitTddPractice/src/algorithm/test/BiggerAndSmallerTest.java
+++ JUnitTddPractice/src/algorithm/test/BiggerAndSmallerTest.java
@@ -6,13 +6,52 @@ import org.junit.Assert;
 public class BiggerAndSmallerTest {
 	
 	@Test
-	public void smallAndBig() {
+	public void numbersInIncreasingOrder() {
 		BiggerAndSmaller algorithm = new BiggerAndSmaller();
 		
-		algorithm.find(new int[] {19, 2, 5, 6, 9});
+		algorithm.find(new int[] {2, 3, 4});
 		
-		System.out.println("Hello");
+		Assert.assertEquals(4, algorithm.getBigger());
+		Assert.assertEquals(2, algorithm.getSmaller());
+	}
+	
+	@Test
+	public void numbersInDecreasingOrder() {
+		BiggerAndSmaller algorithm = new BiggerAndSmaller();
+		
+		algorithm.find(new int[] {4, 3, 2});
+		
+		Assert.assertEquals(4, algorithm.getBigger());
+		Assert.assertEquals(2, algorithm.getSmaller());
+	}
+	
+	@Test
+	public void numbersInAnyOrder() {
+		BiggerAndSmaller algorithm = new BiggerAndSmaller();
+		
+		algorithm.find(new int[] {19, 5, 2, 17, 15, 10});
+		
 		Assert.assertEquals(19, algorithm.getBigger());
+		Assert.assertEquals(2, algorithm.getSmaller());
+	}
+	
+	@Test
+	public void onlyOneNumber() {
+		BiggerAndSmaller algorithm = new BiggerAndSmaller();
+		
+		algorithm.find(new int[] {16});
+		
+		Assert.assertEquals(16, algorithm.getBigger());
+		Assert.assertEquals(16, algorithm.getSmaller());
+	}
+	
+	@Test
+	public void repeatedNumbers() {
+		BiggerAndSmaller algorithm = new BiggerAndSmaller();
+		
+		algorithm.find(new int[] {16, 2, 16, 3, 4, 5, 15, 16});
+		
+		Assert.assertEquals(16, algorithm.getBigger());
 		Assert.assertEquals(2, algorithm.getSmaller());
 	}
 
